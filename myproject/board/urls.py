defc3e6a918df0f776353417ecda2d91a9d43142
leedonggyu1848 <leedong1848@gmail.com>
2022-02-15 15:45:47 +0900

    change url name

--- myproject/board/urls.py
+++ myproject/board/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MyApiView.as_view()),
-    path('<int:article_id>', views.IdApiView.as_view())
+    path('/<int:article_id>', views.IdApiView.as_view())
 ]
